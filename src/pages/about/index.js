26be3a0b2a7fae89d4ef60c75d5e36dc2148c412
geunjin <7612gj@gmail.com>
2023-05-17 00:58:01 +0900

    Design blog

---
+++ src/pages/about/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <span>About Me</span>
+      <p>temp</p>
+    </div>
+  );
+}
+
+export const Head = () => <title>About Me</title>;
